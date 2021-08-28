f97fb9fec3931901896e523a19269f8e188d1e1b
Ajay Makode <ajaymakode402@gmail.com>
2021-08-28 22:51:23 +0530

    Update file

--- automation.sh
+++ automation.sh
@@ -36,5 +36,20 @@ aws s3 \
 cp /tmp/Ajay-httpd-logs-* \
 s3://upgrad-ajaymakode
 
+time = $(stat -f "%Sm" -t "%d%m%Y-%H%M" /tmp/Ajay-httpd-logs-*)
+size = $(wc -c /tmp/Ajay-httpd-logs-* | awk '{print $1}')
+
+if [ ! -f /var/www/html/inventory.html ]
+then
+       touch /var/www/html/inventory.html || exit
+       echo "Log Type   Time Created   Type   Size " | cat >> /var/www/html/inventory.html
+else
+	echo "httpd-logs  ${time}  tar  ${size} " | cat >> /var/www/html/inventory.html
+fi
+
+sudo apt install cron
+
+sudo systemctl enable cron
+
 # removing files
 rm -rf /tmp/Ajay*
